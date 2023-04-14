a69f4610cfd3447ef4b351fbca404a350f46b52e
lattiia <india.lattimore@dillards.com>
2023-04-14 11:23:10 -0500

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideStorage,getStorage } from '@angular/fire/storage';
+import { UploadFormComponent } from './components/upload-form/upload-form.component';
+import { UploadListComponent } from './components/upload-list/upload-list.component';
+import { UploadDetailsComponent } from './components/upload-details/upload-details.component';
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadFormComponent,
+    UploadListComponent,
+    UploadDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideStorage(() => getStorage()),
+    provideDatabase(() => getDatabase())
   ],
-  providers: [],
+  providers: [{ provide: FIREBASE_OPTIONS, useValue: environment.firebase }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+    projectId: 'storagetest1-42e00',
+    appId: '1:770856347658:web:83fe90113cc03eba4ed7f3',
+    databaseURL: 'https://storagetest1-42e00-default-rtdb.firebaseio.com',
+    storageBucket: 'storagetest1-42e00.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDifEhgSKKHg8hDPSqSuM0XjoWfgU4FYTA',
+    authDomain: 'storagetest1-42e00.firebaseapp.com',
+    messagingSenderId: '770856347658',
+    measurementId: 'G-SLDLZD3N60',
+  },
+  production: false
+};
